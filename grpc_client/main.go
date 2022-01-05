97c9cdbbd01839f9824e9a2c319221706a97fee8
wangzhe0912 <wangzhe0912@tju.edu.cn>
2022-01-05 12:45:44 +0800

    supply cli tools for test

---
+++ grpc_client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"github.com/qa-tools-family/app-demo/internal/pb"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	address     = "localhost:8081"
+	defaultName = "world"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
